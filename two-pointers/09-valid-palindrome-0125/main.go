35fe3b2e58f888ccd7c98c4cefd9025de419f611
ehggns <emhas@protonmail.com>
2025-03-09 16:33:54 -0300

    adding documentation in 09

--- two-pointers/09-valid-palindrome-0125/main.go
+++ two-pointers/09-valid-palindrome-0125/main.go
@@ -1,9 +1,20 @@
 package main
 
+// isAlphaNumeric verifies if a character (a byte) is alphanumeric and returns a boolean
 func isAlphaNumeric(c byte) bool {
 	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
 }
 
+/*
+isPalindrome verifies if an input string is a palindrome
+(i.e. the same characters from the left to right and right to left)
+It uses the pattern of two pointers, each one starting from an end of the string.
+While the left pointer does not meet the right pointer, it checks if the characters
+are the same from both pointers. If they are not equal, it returns false. While
+they are equal, left pointer advances forward, and right pointer go backward.
+When l == r, the loop stops and it returns true.
+This approach achieves O(n) time complexity with O(1) space complexity.
+*/
 func isPalindrome(s string) bool {
 	l, r := 0, len(s)-1
 
