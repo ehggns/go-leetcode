bd4f61a69bd460e1995f9e393ebbadd6dccb94d2
ehggns <emhas@protonmail.com>
2025-03-09 22:42:59 -0300

    adding 10

--- two-pointers/10-two-sum-ii--input-array-is-sorted-0167/main.go
+++ two-pointers/10-two-sum-ii--input-array-is-sorted-0167/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+/*
+Two Sum solved for an array/slice of ordered integers. It uses two pointer pattern to
+find the indexes of two numbers.
+*/
+func twoSumTwoPointers(nums []int, target int) []int {
+	l, r := 0, len(nums) -1
+	sum := 0
 
+	for l < r {
+		sum = nums[l] + nums[r]
+
+		if sum == target {
+			return []int{l, r}
+		}
+		if sum > target {
+			r--
+		}
+		if sum < target {
+			l++
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	nums := []int{1, 3, 4, 7, 9, 15, 17}
+	target := 11
+	result := twoSumTwoPointers(nums, target)
+
+	for _, v := range result {
+		println("index:", v)
+	}
 }
