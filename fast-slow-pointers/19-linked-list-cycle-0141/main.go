ad8b5b7fcabaaa155b76ab0d2402a59055e79176
ehggns <emhas@protonmail.com>
2025-05-03 15:50:25 -0300

    adding docs in 19

--- fast-slow-pointers/19-linked-list-cycle-0141/main.go
+++ fast-slow-pointers/19-linked-list-cycle-0141/main.go
@@ -5,6 +5,22 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// hasCycle detects if a linked list has a cycle in it.
+// It uses the Floyd's Cycle-Finding Algorithm (also known as "tortoise and hare" algorithm):
+// - Two pointers, slow and fast, start at the head of the list.
+// - The slow pointer moves one step at a time, while the fast pointer moves two steps.
+// - If there's a cycle, the fast pointer will eventually meet the slow pointer.
+// - If there's no cycle, the fast pointer will reach the end of the list.
+//
+// Parameters:
+//   - head: The head node of the linked list.
+//
+// Returns:
+//   - true if there is a cycle in the linked list.
+//   - false if there is no cycle.
+//
+// Time Complexity: O(n), where n is the number of nodes in the linked list.
+// Space Complexity: O(1), only constant extra space is used.
 func hasCycle(head *ListNode) bool {
 	if head == nil {
 		return false
@@ -16,7 +32,8 @@ func hasCycle(head *ListNode) bool {
 		slow = slow.Next
 		fast = fast.Next.Next
 
-		// If slow and fast pointers meet at some moment, there is a cycle in the linked list.
+		// If slow and fast pointers meet at some moment, there is a cycle in the linked list
+		// i.e. both variables are pointing to the same node address in memory.
 		if slow == fast {
 			return true
 		}
@@ -36,4 +53,12 @@ func main() {
 	}
 
 	hasCycle(&head) // Output: false
+	println("After creating a linked list with 5 nodes, the cycle check returns:")
+	println(hasCycle(&head)) // Output: false
+
+	// Creating a cycle for testing
+	curr.Next = head.Next // Creating a cycle by pointing the last node to the second node
+	hasCycle(&head) // Output: true
+	println("After creating a cycle, the cycle check returns:")
+	println(hasCycle(&head)) // Output: true
 }
